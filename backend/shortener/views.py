8eddc3edb20ac313e705b3d4c774675aac066e5c
Roman Vlasenko <klavionik@gmail.com>
2023-03-28 15:00:14 +0300

    Remove deprecated to_target view

--- backend/shortener/views.py
+++ backend/shortener/views.py
@@ -6,18 +6,10 @@ from shortener.models import Link
 from shortener.signals import link_clicked
 
 
-def to_target(request, alias):
-    link = get_object_or_404(Link, pk=alias)
-    link_clicked.send(sender=to_target, link=link, request=request)
-
-    if link.password:
-        return render(request, "shortener/protected_link.html", context={"alias": alias})
-    return redirect(to=link.target_url)
-
-
 class ToTargetView(View):
     def get(self, request, alias):
         link = get_object_or_404(Link, pk=alias)
+        link_clicked.send(sender=ToTargetView, link=link, request=request)
 
         if link.password:
             return render(request, "shortener/protected_link.html", context={"alias": alias})
