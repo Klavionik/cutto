11c4be24b9be95d67300d839e9c20c692095796d
Roman Vlasenko <klavionik@gmail.com>
2023-12-23 02:03:31 +0300

    Add /healthz/ endpoint

--- backend/shortener/urls.py
+++ backend/shortener/urls.py
@@ -1,10 +1,17 @@
+from django.http import HttpResponse
 from django.urls import include, path
 
 from shortener.views import ToTargetView
 
 app_name = "shortener"
 
+
+def healthz(_):
+    return HttpResponse("OK")
+
+
 urlpatterns = [
     path("go/<str:alias>/", ToTargetView.as_view(), name="to_target"),
     path("api/", include("shortener.api.urls")),
+    path("healthz/", healthz, name="health"),
 ]
